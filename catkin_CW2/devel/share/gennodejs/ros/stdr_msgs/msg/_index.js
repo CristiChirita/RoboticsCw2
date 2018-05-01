3d6993361c58e51456bfbeaaf7fc2a4c35a9116d
CristiChirita <CristiChirita@github.com>
2018-05-01 23:06:03 +0000

    Done

---
+++ catkin_CW2/devel/share/gennodejs/ros/stdr_msgs/msg/_index.js
@@ -0,0 +1,96 @@
+
+"use strict";
+
+let RfidSensorMeasurementMsg = require('./RfidSensorMeasurementMsg.js');
+let FootprintMsg = require('./FootprintMsg.js');
+let KinematicMsg = require('./KinematicMsg.js');
+let CO2SensorMeasurementMsg = require('./CO2SensorMeasurementMsg.js');
+let ThermalSensorMeasurementMsg = require('./ThermalSensorMeasurementMsg.js');
+let ThermalSensorMsg = require('./ThermalSensorMsg.js');
+let RfidTag = require('./RfidTag.js');
+let LaserSensorMsg = require('./LaserSensorMsg.js');
+let CO2SourceVector = require('./CO2SourceVector.js');
+let ThermalSource = require('./ThermalSource.js');
+let RfidSensorMsg = require('./RfidSensorMsg.js');
+let SoundSource = require('./SoundSource.js');
+let SoundSensorMsg = require('./SoundSensorMsg.js');
+let SonarSensorMsg = require('./SonarSensorMsg.js');
+let CO2SensorMsg = require('./CO2SensorMsg.js');
+let SoundSourceVector = require('./SoundSourceVector.js');
+let RobotIndexedVectorMsg = require('./RobotIndexedVectorMsg.js');
+let ThermalSourceVector = require('./ThermalSourceVector.js');
+let RfidTagVector = require('./RfidTagVector.js');
+let RobotMsg = require('./RobotMsg.js');
+let Noise = require('./Noise.js');
+let SoundSensorMeasurementMsg = require('./SoundSensorMeasurementMsg.js');
+let RobotIndexedMsg = require('./RobotIndexedMsg.js');
+let CO2Source = require('./CO2Source.js');
+let RegisterRobotActionGoal = require('./RegisterRobotActionGoal.js');
+let DeleteRobotActionGoal = require('./DeleteRobotActionGoal.js');
+let DeleteRobotActionResult = require('./DeleteRobotActionResult.js');
+let SpawnRobotActionGoal = require('./SpawnRobotActionGoal.js');
+let RegisterRobotFeedback = require('./RegisterRobotFeedback.js');
+let SpawnRobotActionResult = require('./SpawnRobotActionResult.js');
+let SpawnRobotActionFeedback = require('./SpawnRobotActionFeedback.js');
+let SpawnRobotFeedback = require('./SpawnRobotFeedback.js');
+let RegisterRobotAction = require('./RegisterRobotAction.js');
+let SpawnRobotGoal = require('./SpawnRobotGoal.js');
+let DeleteRobotActionFeedback = require('./DeleteRobotActionFeedback.js');
+let DeleteRobotGoal = require('./DeleteRobotGoal.js');
+let RegisterRobotGoal = require('./RegisterRobotGoal.js');
+let SpawnRobotResult = require('./SpawnRobotResult.js');
+let RegisterRobotResult = require('./RegisterRobotResult.js');
+let SpawnRobotAction = require('./SpawnRobotAction.js');
+let DeleteRobotResult = require('./DeleteRobotResult.js');
+let DeleteRobotAction = require('./DeleteRobotAction.js');
+let RegisterRobotActionResult = require('./RegisterRobotActionResult.js');
+let DeleteRobotFeedback = require('./DeleteRobotFeedback.js');
+let RegisterRobotActionFeedback = require('./RegisterRobotActionFeedback.js');
+
+module.exports = {
+  RfidSensorMeasurementMsg: RfidSensorMeasurementMsg,
+  FootprintMsg: FootprintMsg,
+  KinematicMsg: KinematicMsg,
+  CO2SensorMeasurementMsg: CO2SensorMeasurementMsg,
+  ThermalSensorMeasurementMsg: ThermalSensorMeasurementMsg,
+  ThermalSensorMsg: ThermalSensorMsg,
+  RfidTag: RfidTag,
+  LaserSensorMsg: LaserSensorMsg,
+  CO2SourceVector: CO2SourceVector,
+  ThermalSource: ThermalSource,
+  RfidSensorMsg: RfidSensorMsg,
+  SoundSource: SoundSource,
+  SoundSensorMsg: SoundSensorMsg,
+  SonarSensorMsg: SonarSensorMsg,
+  CO2SensorMsg: CO2SensorMsg,
+  SoundSourceVector: SoundSourceVector,
+  RobotIndexedVectorMsg: RobotIndexedVectorMsg,
+  ThermalSourceVector: ThermalSourceVector,
+  RfidTagVector: RfidTagVector,
+  RobotMsg: RobotMsg,
+  Noise: Noise,
+  SoundSensorMeasurementMsg: SoundSensorMeasurementMsg,
+  RobotIndexedMsg: RobotIndexedMsg,
+  CO2Source: CO2Source,
+  RegisterRobotActionGoal: RegisterRobotActionGoal,
+  DeleteRobotActionGoal: DeleteRobotActionGoal,
+  DeleteRobotActionResult: DeleteRobotActionResult,
+  SpawnRobotActionGoal: SpawnRobotActionGoal,
+  RegisterRobotFeedback: RegisterRobotFeedback,
+  SpawnRobotActionResult: SpawnRobotActionResult,
+  SpawnRobotActionFeedback: SpawnRobotActionFeedback,
+  SpawnRobotFeedback: SpawnRobotFeedback,
+  RegisterRobotAction: RegisterRobotAction,
+  SpawnRobotGoal: SpawnRobotGoal,
+  DeleteRobotActionFeedback: DeleteRobotActionFeedback,
+  DeleteRobotGoal: DeleteRobotGoal,
+  RegisterRobotGoal: RegisterRobotGoal,
+  SpawnRobotResult: SpawnRobotResult,
+  RegisterRobotResult: RegisterRobotResult,
+  SpawnRobotAction: SpawnRobotAction,
+  DeleteRobotResult: DeleteRobotResult,
+  DeleteRobotAction: DeleteRobotAction,
+  RegisterRobotActionResult: RegisterRobotActionResult,
+  DeleteRobotFeedback: DeleteRobotFeedback,
+  RegisterRobotActionFeedback: RegisterRobotActionFeedback,
+};
